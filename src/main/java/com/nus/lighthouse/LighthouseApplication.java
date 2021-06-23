2c269002502d69ccb9ab6ee00a741a9f6afff465
truongkimson <sontks@gmail.com>
2021-06-23 14:52:23 +0800

    inital commit

---
+++ src/main/java/com/nus/lighthouse/LighthouseApplication.java
@@ -0,0 +1,13 @@
+package com.nus.lighthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LighthouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LighthouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nus/lighthouse/LighthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nus.lighthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LighthouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

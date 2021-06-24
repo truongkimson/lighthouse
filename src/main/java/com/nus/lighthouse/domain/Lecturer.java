48a9b121a3218a89e24ecfc5c7b7533548ec6a98
truongkimson <sontks@gmail.com>
2021-06-24 15:19:03 +0800

    add lecturer navigation prop

--- src/main/java/com/nus/lighthouse/domain/Lecturer.java
+++ src/main/java/com/nus/lighthouse/domain/Lecturer.java
@@ -3,11 +3,15 @@ package com.nus.lighthouse.domain;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.util.Collection;
 
 @Entity
 @DiscriminatorValue("LEC")
 public class Lecturer extends User{
     private String designation;
+
+    @OneToMany(mappedBy = "lecturer")
+    private Collection<Course> teachCourses;
 
     public Lecturer() {
     }
@@ -15,5 +19,21 @@ public class Lecturer extends User{
     public Lecturer(String email, String password, String firstName, String lastName, String designation) {
         super(email, password, firstName, lastName);
         this.designation = designation;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Collection<Course> getTeachCourses() {
+        return teachCourses;
+    }
+
+    public void setTeachCourses(Collection<Course> teachCourses) {
+        this.teachCourses = teachCourses;
     }
 }

1e5572141062f5b2a1cef4958662f9978b9b8004
truongkimson <60567409+truongkimson@users.noreply.github.com>
2021-06-30 12:12:45 +0800

    fix uniqueEmail validation bug

--- src/main/java/com/nus/lighthouse/repo/UserRepository.java
+++ src/main/java/com/nus/lighthouse/repo/UserRepository.java
@@ -1,11 +1,10 @@
 package com.nus.lighthouse.repo;
 
-import com.nus.lighthouse.domain.Student;
 import com.nus.lighthouse.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //If we intend to do the login methods, we might want to do it here
 public interface UserRepository extends JpaRepository<User,Integer>{
 
-    Student findUserByEmail(String email);
+    User findUserByEmail(String email);
 }

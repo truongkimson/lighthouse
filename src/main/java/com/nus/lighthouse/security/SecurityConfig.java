8cac06299fbaed93441bc55923ef715e5ff539d1
truongkimson <sontks@gmail.com>
2021-07-01 17:01:07 +0800

    fix cannot load static resources

--- src/main/java/com/nus/lighthouse/security/SecurityConfig.java
+++ src/main/java/com/nus/lighthouse/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -46,6 +47,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .invalidateHttpSession(true)
 //                .deleteCookies("JSSESSIONID");
     }
+
+    @Override
+    public void configure(final WebSecurity web) {
+        web.ignoring().antMatchers("/css/**", "/images/**");
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {

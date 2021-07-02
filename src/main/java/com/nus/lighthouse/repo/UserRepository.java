20fc297c28ebfc5dc212a0da0d75b0617ed155f7
truongkimson <60567409+truongkimson@users.noreply.github.com>
2021-07-02 11:30:44 +0800

    use default 'findUserByEmail' in UserRepository.java

--- src/main/java/com/nus/lighthouse/repo/UserRepository.java
+++ src/main/java/com/nus/lighthouse/repo/UserRepository.java
@@ -2,13 +2,11 @@ package com.nus.lighthouse.repo;
 
 import com.nus.lighthouse.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 //If we intend to do the login methods, we might want to do it here
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>{
-	@Query(value="Select u from User u where u.email=:email")
-    User findUserByEmail(@Param("email")String email);
+
+    User findUserByEmail(String email);
 }
